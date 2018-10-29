5dcbe233c6e2b4ab73283087aa5f81cd61241892
Moni888 <ochhh@op.pl>
2018-10-29 21:21:28 +0100

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_02_170044) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
